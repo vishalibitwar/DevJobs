3e0d7e9143343134f95adb5244633caa03eef060
vishalibitwar <vish31ibitwar@gmail.com>
2020-05-15 15:58:08 +0530

    Updates

---
+++ precache-manifest.87860fb520c53f1b7b41e0ae4568a892.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06c2c3610517df760720f8db2c368861",
+    "url": "/DevJobs/index.html"
+  },
+  {
+    "revision": "60c2a3ef50b78481f080",
+    "url": "/DevJobs/static/css/main.d603b91f.chunk.css"
+  },
+  {
+    "revision": "5411695b83e897715609",
+    "url": "/DevJobs/static/js/2.6d0adf02.chunk.js"
+  },
+  {
+    "revision": "60c2a3ef50b78481f080",
+    "url": "/DevJobs/static/js/main.e5a86dd4.chunk.js"
+  },
+  {
+    "revision": "352288459c427288cd15",
+    "url": "/DevJobs/static/js/runtime-main.8fea701b.js"
+  }
+]);
